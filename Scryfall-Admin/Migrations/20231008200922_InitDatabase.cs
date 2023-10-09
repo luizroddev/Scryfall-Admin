9a2c9242707b6f82d90a073dba94a676315dcb96
luizroddev <luizwippe@icloud.com>
2023-10-09 18:04:46 -0300

    fix: removed unused migrations and renewed database model

---
+++ Scryfall-Admin/Migrations/20231008200922_InitDatabase.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Scryfall_Admin.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cartas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Mana = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CustoDeMana = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Tipo = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Texto = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Poder = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Resistencia = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Lealdade = table.Column<int>(type: "NUMBER(10)", nullable: true),
+                    FlavorText = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Raridade = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    LegalidadesId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    ImagemUrisId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cartas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ImageUris",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Small = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Normal = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Large = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CartaId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageUris", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ImageUris_Cartas_CartaId",
+                        column: x => x.CartaId,
+                        principalTable: "Cartas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Legalidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Standard = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Modern = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Legacy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Pauper = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Duel = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Predh = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CartaId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Legalidades", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Legalidades_Cartas_CartaId",
+                        column: x => x.CartaId,
+                        principalTable: "Cartas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageUris_CartaId",
+                table: "ImageUris",
+                column: "CartaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Legalidades_CartaId",
+                table: "Legalidades",
+                column: "CartaId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImageUris");
+
+            migrationBuilder.DropTable(
+                name: "Legalidades");
+
+            migrationBuilder.DropTable(
+                name: "Cartas");
+        }
+    }
+}
---
+++ Scryfall-Admin/Migrations/20231008201523_RefactorForeignKeys.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Scryfall_Admin.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorForeignKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImageUris_Cartas_CartaId",
+                table: "ImageUris");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Legalidades_Cartas_CartaId",
+                table: "Legalidades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Legalidades_CartaId",
+                table: "Legalidades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImageUris_CartaId",
+                table: "ImageUris");
+
+            migrationBuilder.DropColumn(
+                name: "CartaId",
+                table: "Legalidades");
+
+            migrationBuilder.DropColumn(
+                name: "CartaId",
+                table: "ImageUris");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cartas_ImagemUrisId",
+                table: "Cartas",
+                column: "ImagemUrisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cartas_LegalidadesId",
+                table: "Cartas",
+                column: "LegalidadesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cartas_ImageUris_ImagemUrisId",
+                table: "Cartas",
+                column: "ImagemUrisId",
+                principalTable: "ImageUris",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cartas_Legalidades_LegalidadesId",
+                table: "Cartas",
+                column: "LegalidadesId",
+                principalTable: "Legalidades",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cartas_ImageUris_ImagemUrisId",
+                table: "Cartas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cartas_Legalidades_LegalidadesId",
+                table: "Cartas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cartas_ImagemUrisId",
+                table: "Cartas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cartas_LegalidadesId",
+                table: "Cartas");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CartaId",
+                table: "Legalidades",
+                type: "NUMBER(10)",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CartaId",
+                table: "ImageUris",
+                type: "NUMBER(10)",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Legalidades_CartaId",
+                table: "Legalidades",
+                column: "CartaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageUris_CartaId",
+                table: "ImageUris",
+                column: "CartaId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImageUris_Cartas_CartaId",
+                table: "ImageUris",
+                column: "CartaId",
+                principalTable: "Cartas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Legalidades_Cartas_CartaId",
+                table: "Legalidades",
+                column: "CartaId",
+                principalTable: "Cartas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
